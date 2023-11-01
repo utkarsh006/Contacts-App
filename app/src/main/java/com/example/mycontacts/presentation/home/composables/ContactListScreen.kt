fead0f688d14068ee2d772b1a27bf4ea5a25237e
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-11-01 20:19:09 +0530

    made search ui

--- app/src/main/java/com/example/mycontacts/presentation/home/composables/ContactListScreen.kt
+++ app/src/main/java/com/example/mycontacts/presentation/home/composables/ContactListScreen.kt
@@ -1,21 +1,82 @@
 package com.example.mycontacts.presentation.home.composables
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycontacts.domain.model.Contact
 
 @Composable
 fun ContactList(
     contactList: List<Contact>,
     onContactClick: (Contact) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    search: String,
+    onValueChange: (String) -> Unit
 ) {
-    LazyColumn(
+    TextField(
+        value = search,
+        onValueChange = onValueChange,
+        modifier = modifier.fillMaxWidth(),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = Color.White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
 
-    ) {
+        placeholder = {
+            Text(
+                text = "Search a Contact",
+                style = TextStyle(
+                    color = Color.DarkGray,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            )
+        },
+
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Outlined.Search,
+                contentDescription = "",
+                tint = Red
+            )
+        },
+
+        trailingIcon = {
+            if (search.isNotEmpty())
+                IconButton(onClick = { onValueChange("") }) {
+                    Icon(
+                        imageVector = Icons.Outlined.Close, contentDescription = "", tint = Red
+                    )
+                }
+        },
+        shape = RoundedCornerShape(10.dp)
+    )
+
+    Spacer(modifier = Modifier.height(8.dp))
+
+    LazyColumn() {
         items(items = contactList, key = { it.id }) { contact ->
             ContactItems(
                 contact = contact,
@@ -24,4 +85,15 @@ fun ContactList(
             Divider()
         }
     }
+}
+
+@Preview
+@Composable
+fun ContactListPreview() {
+    ContactList(
+        contactList = listOf(),
+        onContactClick = {},
+        search = "Utkarsh",
+        onValueChange = {}
+    )
 }
